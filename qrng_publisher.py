6259027900beb0e02d9cd964e90ed257ab19a0f2
francescosisini <francescomichelesisini@gmail.com>
2024-09-02 14:24:53 +0200

    Tes

--- qrng_publisher.py
+++ qrng_publisher.py
@@ -1,10 +1,11 @@
 import random
+import logging
 from google.cloud import pubsub_v1
 import time
 
 # Configurazione del publisher
-project_id = "servizi"
-topic_id = "projects/strange-mind-312015/topics/QGRN"
+project_id = "strange-mind-312015"
+topic_id = "QGRN"
 
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_id)
@@ -15,9 +16,14 @@ def get_random_number_from_qrng():
     return str(random.randint(0, 100))
 
 def publish_random_number():
-    random_number = get_random_number_from_qrng()
-    future = publisher.publish(topic_path, random_number.encode("utf-8"))
-    print(f"Published message: {random_number}")
+     try:
+        random_number = get_random_number_from_qrng()
+        future = publisher.publish(topic_path, random_number.encode("utf-8"))
+        logging.info(f"Published message: {random_number}")
+        future.result()  # Attende che il messaggio venga inviato
+     except Exception as e:
+        logging.error(f"Failed to publish message: {e}")
+        
 
 #while True:
 publish_random_number()
